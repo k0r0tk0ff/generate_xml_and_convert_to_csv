8b50d71f872f76729d4739b5f78a065936ea8592
Alexandr <korotkov34reg@gmail.com>
2018-10-03 13:37:31 +0300

    Create StorageException.java

---
+++ src/main/java/ru/k0r0tk0ff/storage/StorageException.java
@@ -0,0 +1,23 @@
+package ru.k0r0tk0ff.storage;
+
+public class StorageException extends Exception {
+    public StorageException() {
+        super();
+    }
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StorageException(Throwable cause) {
+        super(cause);
+    }
+
+    protected StorageException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
